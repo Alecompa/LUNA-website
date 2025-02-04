1a94665754ab966bc90015b8ca530f26f5707658
Alecompa <alessandro.compagnucci@gmail.com>
2025-02-04 16:07:33 +0100

    Added analytics and speed insights from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Inter } from "next/font/google"
 import Header from "./components/header"
 import Footer from "./components/footer"
 import type React from "react" // Added import for React
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -26,8 +28,11 @@ export default function RootLayout({
         <Header />
         <main>{children}</main>
         <Footer />
+        <Analytics />
+        <SpeedInsights />
       </body>
     </html>
+
   )
 }
 
